dd6dd03d7f1921be9849bfaf926b3bcc3fc51bbb
Cadu 79 <eduardoprimogenito@gmail.com>
2024-05-24 12:14:49 -0300

    setup do exercicio_ts

---
+++ src/exercicio.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+const resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá" + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao);
+
+const saudacao = saudar("Maria");
+console.log(saudacao);
